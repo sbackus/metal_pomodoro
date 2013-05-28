85eb643c6a0cbec7882a9862fe4f641bb8986f8f
Sam Backus <Samuel.Backus@gmail.com>
2013-05-27 21:12:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetalPomodoro::Application.config.secret_token = '9bdfbe232ee1ae36f6dc44a238f27f5b6ed131d50369b6f7ef08e294acf6ff8ee27963415947cdf0c268e6f0e4c5ede8b5f561ad6527fdb368565c8219078085'
