a7a3ea0cd2c9ecde68504b3c4fcb4e5c3dd63244
Sam Backus <Samuel.Backus@gmail.com>
2013-05-29 16:34:13 -0400

    add guard and spork

--- spec/features/metal_pomodoro_spec.rb
+++ spec/features/metal_pomodoro_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Metal Pomodoro" do
 	subject {page}
-	before {visit root_path}
+	before {visit root_path} 
   
   it { should have_selector 'title', text: 'Metal Pomodoro - The most EPIC pomodoro EVER' }	
 end
